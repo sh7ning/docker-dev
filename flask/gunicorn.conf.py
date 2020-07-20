430779f92d5dcbfb97dcac7152dc7de5e6f8a211
sh7ning <sh7ning@qq.com>
2020-07-20 20:32:03 +0800

    support flask

---
+++ flask/gunicorn.conf.py
@@ -0,0 +1,10 @@
+import multiprocessing
+
+# see: https://docs.gunicorn.org/en/latest/settings.html#settings
+bind = "0.0.0.0:5000"
+# workers = multiprocessing.cpu_count() * 2 + 1
+workers = 2
+worker_class = "gevent" # 默认为阻塞模式，最好选择gevent模式
+loglevel = 'info'
+# accesslog=
+# errorlog=
